2c3da47ca0367509e6ed70787f04113360fff789
JeanCarlos09 <136395052+JeanCarlos09@users.noreply.github.com>
2023-06-21 23:47:46 -0500

    Delete IPizzaBuilder.cs

--- IPizzaBuilder.cs
+++
@@ -1,8 +0,0 @@
-// Paso 2: Crea una interfaz o clase abstracta para el constructor
-public interface IPizzaBuilder
-{
-    void ConstruirTipoMasa();
-    void ConstruirSalsa();
-    void ConstruirTipo();
-    Pizza ObtenerPizza();
-}
